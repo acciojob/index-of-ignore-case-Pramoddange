51e1076f6e2c124bc00eaf9746632ce4bf7e51c9
Pramod Dange <123957464+Pramoddange@users.noreply.github.com>
2024-06-01 12:06:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
 	
-	return s1.indexOf(s2.charAt(0));
+console.log(s1.toLowerCase().indexOf(s2.toLowerCase().charAt(0)));
 }
-
 //Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
