8989280a5187007b5870042010deaff8c47296cc
Pramod Dange <123957464+Pramoddange@users.noreply.github.com>
2024-06-01 12:08:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
-	
-console.log(s1.toLowerCase().indexOf(s2.toLowerCase().charAt(0)));
+  // write your code her
+	return (s1.toLowerCase().indexOf(s2.toLowerCase().charAt(0)));
 }
-//Please do not change the code below
+///Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
